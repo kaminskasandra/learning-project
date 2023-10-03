f743efdb359ebd1a3988c8b12d0ef426661125e2
kaminskasandra <san.porombka@gmail.com>
2023-10-03 12:11:37 +0200

    .\src\

---
+++ src/Application.java
@@ -0,0 +1,11 @@
+public class Application {
+    public static void main(String[] args) {
+
+        Calculator calculator = new Calculator();
+        int result = calculator.addAtoB(7, 9);
+
+
+        System.out.println(result);
+
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,22 +1,7 @@
-import java.sql.SQLOutput;
-import java.util.Scanner;
 
-public class Main {
-    public static void main(String[] args) {
+class Calculator {
 
-        Scanner input = new Scanner(System.in);
-        System.out.println("Proszę podać swoje imię");
-
-        String name = input.next();
-
-        System.out.println("Proszę podać swój wiek");
-
-        int age = input.nextInt();
-
-        System.out.println("Proszę podać swoją pensję");
-
-        double salary = input.nextDouble();
-
-        System.out.println("Twoja pensja to: " + salary);
+    public int addAtoB(int a, int b) {
+        return a + b;
     }
 }
